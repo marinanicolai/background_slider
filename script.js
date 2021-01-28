0f096d5d4547fb6bbb4b43e645a79ad9ea227f2c
marina <nicolaimarina@gmail.com>
2021-01-28 14:23:44 -0500

    js update

---
+++ script.js
@@ -0,0 +1,30 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlider++
+
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach(slide =>
+        slide.classList.remove('active')
+    )
+    slides(activeSlide).classList.add('active')
+}
